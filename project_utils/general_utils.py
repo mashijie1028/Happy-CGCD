56a45782510bb6c993dc0349206f2261cf3143b7
Shijie Ma <mashijie2021@ia.ac.cn>
2024-10-08 22:37:32 +0800

    Add files via upload

--- project_utils/general_utils.py
+++ project_utils/general_utils.py
@@ -83,13 +83,15 @@ def init_experiment(args, runner_name=None, exp_id=None):
         runner_name = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))).split(".")[-2:]
 
     #root_dir = os.path.join(args.exp_root, *runner_name)
-    root_dir = os.path.join(args.exp_root, args.dataset_name)
+    # NOTE!!! add shuffle to path
+    if args.shuffle_classes:
+        root_dir = os.path.join(args.exp_root, args.dataset_name + '_shuffle_seed_' + str(args.seed))
+    else:
+        root_dir = os.path.join(args.exp_root, args.dataset_name)
 
     if not os.path.exists(root_dir):
         os.makedirs(root_dir)
 
-    if args.exp_name is None:
-        raise ValueError("Need to specify the experiment name")
     # Unique identifier for experiment
     now = str(time.strftime("%Y%m%d-%H%M%S", time.localtime()))
     if args.base_exp_id is not None:
@@ -109,152 +111,7 @@ def init_experiment(args, runner_name=None, exp_id=None):
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
 
-
-    #logger.add(os.path.join(log_dir, 'log.txt'))
     logger.add(os.path.join(log_dir, 'log.txt'))
-    args.logger = logger
-    args.log_dir = log_dir
-
-    # Instantiate directory to save models to
-    model_root_dir = os.path.join(args.log_dir, 'checkpoints')
-    if not os.path.exists(model_root_dir):
-        os.mkdir(model_root_dir)
-
-    args.model_dir = model_root_dir
-    args.model_path = os.path.join(args.model_dir, 'model.pt')
-
-    print(f'Experiment saved to: {args.log_dir}')
-    print(runner_name)
-
-    # print and save args
-    print(args)
-    save_args_path = os.path.join(log_dir, 'args.txt')
-    f_args = open(save_args_path, 'w')
-    f_args.write('args: \n')
-    f_args.write(str(vars(args)))
-    f_args.close()
-
-    return args
-
-
-def init_experiment_with_load_continual(args, runner_name=None, exp_id=None):
-    # Get filepath of calling script
-    if runner_name is None:
-        runner_name = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))).split(".")[-2:]
-
-    #root_dir = os.path.join(args.exp_root, *runner_name)
-    # NOTE!!! add shuffle to path
-    if args.shuffle_classes:
-        root_dir = os.path.join(args.exp_root, args.dataset_name + '_shuffle_seed_' + str(args.seed))
-    else:
-        root_dir = os.path.join(args.exp_root, args.dataset_name)
-
-    if not os.path.exists(root_dir):
-        os.makedirs(root_dir)
-
-    # Unique identifier for experiment
-    now = str(time.strftime("%Y%m%d-%H%M%S", time.localtime()))
-    if args.base_exp_id is not None:
-        now = args.base_exp_id + '_' + now
-
-    log_dir = os.path.join(root_dir, now)
-    while os.path.exists(log_dir):
-        now = str(time.strftime("%Y%m%d-%H%M%S", time.localtime()))
-        if args.base_exp_id is not None:
-            now = args.base_exp_id + '_' + now
-
-        log_dir = os.path.join(root_dir, now)
-
-    # load
-    if args.load_continual:
-        now = args.load_online_id
-        log_dir = os.path.join(root_dir, now)
-
-    if exp_id is not None:
-        log_dir = log_dir + '_' + exp_id
-
-    if not os.path.exists(log_dir):
-        os.makedirs(log_dir)
-
-    # load
-    if args.load_continual:
-        logger.add(os.path.join(log_dir, 'log_load_continual.txt'))
-    else:
-        logger.add(os.path.join(log_dir, 'log.txt'))
-    args.logger = logger
-    args.log_dir = log_dir
-
-    # Instantiate directory to save models to
-    model_root_dir = os.path.join(args.log_dir, 'checkpoints')
-    if not os.path.exists(model_root_dir):
-        os.mkdir(model_root_dir)
-
-    args.model_dir = model_root_dir
-    args.model_path = os.path.join(args.model_dir, 'model.pt')
-
-    print(f'Experiment saved to: {args.log_dir}')
-    print(runner_name)
-
-    # print and save args
-    print(args)
-    save_args_path = os.path.join(log_dir, 'args.txt')
-    f_args = open(save_args_path, 'w')
-    f_args.write('args: \n')
-    f_args.write(str(vars(args)))
-    f_args.close()
-
-    return args
-
-
-def init_experiment_with_load_continual_offline(args, runner_name=None, exp_id=None):
-    # Get filepath of calling script
-    if runner_name is None:
-        runner_name = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))).split(".")[-2:]
-
-    #root_dir = os.path.join(args.exp_root, *runner_name)
-    # NOTE!!! add shuffle to path
-    if args.shuffle_classes:
-        root_dir = os.path.join(args.exp_root, args.dataset_name + '_shuffle_seed_' + str(args.seed))
-    else:
-        root_dir = os.path.join(args.exp_root, args.dataset_name)
-
-    if not os.path.exists(root_dir):
-        os.makedirs(root_dir)
-
-    # Unique identifier for experiment
-    now = str(time.strftime("%Y%m%d-%H%M%S", time.localtime()))
-    if args.base_exp_id is not None:
-        now = args.base_exp_id + '_' + now
-
-    log_dir = os.path.join(root_dir, now)
-    while os.path.exists(log_dir):
-        now = str(time.strftime("%Y%m%d-%H%M%S", time.localtime()))
-        if args.base_exp_id is not None:
-            now = args.base_exp_id + '_' + now
-
-        log_dir = os.path.join(root_dir, now)
-
-    # load
-    if args.load_continual:
-        now = args.load_online_id
-        log_dir = os.path.join(root_dir, now)
-
-    # NOTE!!! load offline
-    if args.load_offline_id_retrain is not None:
-        now = args.load_offline_id_retrain
-        log_dir = os.path.join(root_dir, now)
-
-    if exp_id is not None:
-        log_dir = log_dir + '_' + exp_id
-
-    if not os.path.exists(log_dir):
-        os.makedirs(log_dir)
-
-    # load
-    if args.load_continual or (args.load_offline_id_retrain is not None):   # NOTE!!!
-        logger.add(os.path.join(log_dir, 'log_load_continual.txt'))
-    else:
-        logger.add(os.path.join(log_dir, 'log.txt'))
     args.logger = logger
     args.log_dir = log_dir
 
